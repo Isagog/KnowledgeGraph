a3a3af64fa4972acdad910c81539c34e21ff2543
Guido Vetere <g.vetere@isagog.com>
2021-04-13 18:40:07 +0200

    Restructure project

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd kg-java-client || exit 1
+mvn clean install
+echo "API CLIENT INSTALLED"
+cd ../kg-msf4j-server || exit 1
+mvn clean compile
+mvn install -Dmaven.main.skip=true
+echo "SERVER STUB INSTALLED"
+cd ../kg-service || exit 1
+mvn package
+echo "SERVICE PACKAGED"
+cd ../
+echo "DONE"
+sleep 3
+
