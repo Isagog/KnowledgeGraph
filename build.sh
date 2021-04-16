2d0aec3b0d9e3b146c178dcabed3e7d2bf87f44e
Guido Vetere <g.vetere@isagog.com>
2021-04-16 10:37:35 +0200

    Fix build

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ mvn clean compile
 mvn install -Dmaven.main.skip=true
 echo "SERVER STUB INSTALLED"
 cd ../kg-service || exit 1
-mvn package
+mvn clean package
 echo "SERVICE PACKAGED"
 cd ../
 echo "DONE"
